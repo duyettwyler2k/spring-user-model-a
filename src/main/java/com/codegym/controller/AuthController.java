08eff8680e03e3ad1a46b3732259bcea884206e2
duyettwyler2k <duyettwyller@gmail.com>
2021-06-16 13:32:04 +0700

    'ads'

---
+++ src/main/java/com/codegym/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.codegym.controller;
+
+import com.codegym.model.User;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Controller
+
+public class AuthController {
+    @GetMapping("/register")
+    public String showRegisterForm(Model model){
+        model.addAttribute("user",new User());
+        return "index";
+    }
+    @PostMapping("/register")
+    public ModelAndView register(@ModelAttribute(name="user")User user){
+        ModelAndView modelAndView = new ModelAndView("result");
+        modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/result.jsp
@@ -0,0 +1,14 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>User info</h1>
+<p>Username: ${user.username}</p>
+<p>Password: ${user.password}</p>
+<p>Email: ${user.email}</p>
+<p>Phone Number: ${user.phoneNumber}</p>
+</body>
+</html>
